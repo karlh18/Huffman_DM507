d0719bcfdacbeb525c4fef5e4f39d0aa3cc8dfc6
karlh18 <43171793+karlh18@users.noreply.github.com>
2020-05-07 19:29:46 +0100

    Cleaned up the code

--- Huffman_DM507/src/Proj_3/Encode.java
+++ Huffman_DM507/src/Proj_3/Encode.java
@@ -7,63 +7,59 @@ package Proj_3;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
- * @author Karla
+ * @author Karl Amadeus Finsson Hansen - karlh18@student.sdu.dk
+ * @author Pradeep Thayaparan - prtha18@student.sdu.dk
+ * @author Lavan Sathiyaseelan - lasat17@student.sdu.dk
  */
-public class Encode {    
-    
-    
-  //  BinNode HNode = new BinNode(); 
+public class Encode {
+
     PQ z = new PQHeap();
     int counter = 0;
-    int[] alphabet = new int[256];
+    int[] alphabet = new int[256]; // Could also just return it via countFrequencyOfBytes 
     int[] isIndex = new int[256];   // used to compare
-    //  int[] tree = new int[256];  // huffmans converted tree --> table 
 
-   // PQHeap Q;
-    
-//    public Encode(String fil) throws IOException {
-//        
-//        countBytes(fil); 
-//        
-//    }
-//    
-    public static void main(String[] args) throws IOException {  
-     Encode en = new Encode();  
-     
-     en.countBytes(args);
-     
-        
-        
-        int d = (int) 'd';
-        System.out.println("Number: " + d);
-//
-//        alphabet[d]++;
-//        
-//
-//        System.out.println(alphabet[d]);
-//  int nextBit = 0;
-//  int output = 2;  
-//        System.out.println( 1 | nextBit);
-//     output = output << 1 | nextBit; 
-//        System.out.println("Annnnnnnddd   "+output);
-        //      System.out.println(d);
+    public static void main(String[] args) throws IOException {
+        Encode encode = new Encode();
+        // Read a file And put it into another file
+        encode.countFrequencyOfBytes(args[0]);
+
+    }
+
+    // Task 1)  Read a file and make a table of how often a given byte occurs in the file
+    public void countFrequencyOfBytes(String inputFile) throws IOException {
+        // Used to temporary store the current read()
+        int temp;
+        // Opens File to read from 
+        try (FileInputStream input = new FileInputStream(inputFile)) {
+            // "For at læse bytes fra en fil, skal man bruge read-metoden fra FileInputStream" 
+            // This read method reads 1 byte instead of 4 bytes like the read method of the library class BitINputstream 
+            while ((temp = input.read()) != 0) {
+                // int temp = (int) flin.read(); 
+                System.out.println(temp);
+                alphabet[temp]++;
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
-    public void huffMan(int[] alphabet) {
+    
+    //Task 2) Use the Huffman Algorithm with the Occurrence Table as input ( Use all 256 entries, also those, that do not ocur) 
+    public void huffmanAlgorithm(int[] alphabet) {
 
         int n = alphabet.length;
         // Q = new PQHeap();
         PQ Q = new PQHeap();
 
         for (int i = 0; i < n - 1; i++) {
-     
+
 //            Q.insert(new Element(, i));
         }
 
@@ -77,72 +73,6 @@ public class Encode {
 //               return z.extractMin();
         }
 
-
-
     }
-    
-    public void insertIntoAlphath(String File) {
-        
-        for (int i = 0; i < alphabet.length; i++) {
-            
-            int k = alphabet[i];            
-            
-            byte b = (byte) i;            
-            
-            System.out.println("b = " + b);   //-1 because out of range   
-            
-            int bigbyte = b & 0xFF;
-            
-            System.out.println("bigbyte = " + b);   //-1 because out of range   
-            
-            if (alphabet[i] == isIndex[i]) {
-                alphabet[i]++;                
-                counter++;                
-            }
-            
-        }
-    }
-    Scanner sc;
-    
-    FileInputStream flin;
 
-    // Opgave 1:
-    // Læs en fil ind
-    // for hvert byte (aka for hvert bogstav) tæl, hvor often den forekommer 
-    // d = (int) 'd' 
-    // alphabet[d} ++; 
-    public void countBytes(String[] a) throws IOException {  
-
-        int  tempr = 0; 
-        // Freq table  --> temp value      
-        try ( FileInputStream flin = new FileInputStream(a[0])) {
-
-            // flin = new FileInputStream("D:\\isnfaos");
-            // Wrap the new bit streams around the input/output streams.
-            BitInputStream bitInputStream = new BitInputStream(flin); 
-            while((tempr = flin.read()) != 0) {  
-               // int temp = (int) flin.read(); 
-               alphabet[tempr]++;
-               
-            
-            }
-         
-            // need to use another file aka outfile.
-            // BitOutputStream bitOutputStream = new BitOutputStream();
-            // First read a full int (i.e., four bytes) from input file
-            // using the library method readInt().
-            int i = bitInputStream.readInt();
-             
-            
-            //int i = flin.read();   
-            alphabet[i]++;
-
-//            System.out.println((char) i);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        flin.close();
-    }
-    
 }
