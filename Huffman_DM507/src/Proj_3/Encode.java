d065d05ad84d05754c1aedf0de1fee7f15b22e44
karlh18 <43171793+karlh18@users.noreply.github.com>
2020-05-08 22:26:45 +0100

    __Cleaned A bit up

--- Huffman_DM507/src/Proj_3/Encode.java
+++ Huffman_DM507/src/Proj_3/Encode.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Proj_3;
 
 import java.io.FileInputStream;
@@ -10,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,23 +15,114 @@ import java.util.logging.Logger;
  */
 public class Encode {
 
+    
+    // Stores each byte as an int
+    int[] alphabet = new int[256];
+
+    // Used to store the prefix code for each byte
+    String[] codeLookupTable;
+    
+    // HuffMan Tree
+    DictBinTree huffManTree;
+    
+    // Task 1)  Reads a file and makes a table of how often a given byte occurs in the file    
+    public void countFrequencyOfBytes(String inputFile) throws IOException {
+        // Used to temporary store the current read()
+        int temp;
+
+        // Opens File to read from 
+        try (FileInputStream input = new FileInputStream(inputFile)) {
+            // Use the read method from FileInputStream  to read a byte (8bits) from a file
+            // This read method reads 1 byte instead of 4 bytes like the read method of the library class BitInputstream 
+            while ((temp = input.read()) != -1) {
+                System.out.println(temp); // used to test if it reads correct
+                alphabet[temp]++;
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    //Task 2) Use the Huffman Algorithm with the Occurrence Table as input ( Use all 256 entries, also those, that do not occur  
+    // Creates a HuffmanTree 
+     public BinNode huffmanAlgorithm(int[] alphabet) {
+
+        int n = alphabet.length;
+
+        //minHeap
+        PQ priorityQueue = new PQHeap();
+
+        // Q = C 
+        // Initialize the Priority Queue with the Characters 
+        for (int i = 0; i < n - 1; i++) {
+
+            BinNode node = new BinNode(i);
+
+            // Add element into tree? 
+            node.key = i ; // Not sure if correct          // Should this be changed ? 
+
+            // Add a tree
+            Element e = new Element(alphabet[i], node);
+
+            priorityQueue.insert(e);
+
+        }
+
+        // for i = 1 to n-1 
+        // Not entirely sure how it translates from pseudo to our yet. So I will just minus it as usual 
+        // I just guess we start @Index 0  and then move to second last index which is length -2 in java ArrayList
+        for (int i = 0; i < n - 2; i++) {
+
+            // Doesn't this provide us with a tree instead of a node?
+            // Should be tree or node? hmmm
+            // Extract the elements with the lowest key in the heap
+            // and get their sum
+            Element x = priorityQueue.extractMin();
+            Element y = priorityQueue.extractMin();
+            int sum = x.getKey() + y.getKey();
+            System.out.println(sum);
+            // Used later to get the nodes left 
+            BinNode xx = (BinNode) x.getData();
+            BinNode yy = (BinNode) y.getData();
+
+            // Creates new tree & adds the two previous as children
+            BinNode z = new BinNode(sum);
+            z.binNodeLeft =  (BinNode) x.getData();
+            z.binNodeRight =(BinNode) y.getData();
+
+            // Adds the new tree to the priority queue
+            priorityQueue.insert(new Element(sum, z));
+        }
+
+        return (BinNode) priorityQueue.extractMin().getData();
+    }
+
+    
+    
+    
+    
+    // Remember to change arguments in methods to args[0] for inputfile & args[1]for outputfile
     public static void main(String[] args) throws IOException { 
-       // TEST OF countFrequencyOfBytes
-       String s = "hej.txt";
+       // TEST OF countFrequencyOfBytes Method
+        String s = "hej.txt";
         Encode encode = new Encode();
         encode.countFrequencyOfBytes(s);  
 //        System.out.println(encode.alphabet[104]);
 //        System.out.println(encode.alphabet[107]); 
 //        
 
+
+
+
+
       // TEST OF huffmanAlgorithm  
-      BinNode binNode = encode.huffmanAlgorithm2(encode.alphabet);  
+ //     BinNode binNode = encode.huffmanAlgorithm2(encode.alphabet);  
        
-       DictBinTree dictBinTree = new DictBinTree();
+//       DictBinTree dictBinTree = new DictBinTree();
        
-       dictBinTree.root = binNode;
+      // dictBinTree.root = binNode;
        
-       dictBinTree.in_order_walk_with_path();
+//       dictBinTree.in_order_walk_with_path();
        
      // TEST OF SAVECODE  
      
@@ -48,35 +133,72 @@ public class Encode {
 //        System.out.println(alphabet[3]);
     }
 
-    String[] codelookup;
-    PQ z = new PQHeap();
-    int counter = 0;
-    int[] alphabet = new int[256]; // Could also just return it via countFrequencyOfBytes 
-    int[] isIndex = new int[256];   // used to compare
+    
+    
+    
+    
+    
+    
+    
 
-    // Task 1)  Read a file and make a table of how often a given byte occurs in the file
-    public void countFrequencyOfBytes(String inputFile) throws IOException {
-        // Used to temporary store the current read()
-        int temp;  
-        
-       
-        
+    
+    //Task 3 )  Convert the Huffman Tree to a Table(An Array with 256 entries)
+    // Over the codes for each of the possible bytes 
+    // Remember - The bytes are of the type int between 0 and 255 
+    // and can be used as indexes in the arrays
+    public String[] saveCode(DictBinTree tree) {
+        ArrayList<String> code = tree.in_order_walk_with_path();
+        String[] codeArray = new String[256];
+        // make toArray later
+
+        for (int i = 0; i < code.size() - 1; i++) {
+            codeArray[i] = code.get(i);  
+            System.out.println(codeArray[i]);
+        }
+        codeLookupTable = codeArray;
+        return codeArray;
+    }
+
+    //task 4)  Write alphabeth into outputfile
+    public void writeIntsTOoOutput(int[] alphabeth, String outFile) throws IOException {
+        try ( FileOutputStream output = new FileOutputStream(outFile);  BitOutputStream out = new BitOutputStream(output);) {
+            for (int i : alphabeth) {
+                out.writeInt(i);
+            }
+        }
+
+    }
+
+    //task 5)   
+    public void WriteInput(String inputFile, String outputFile) throws IOException {
+
+        // Vi skal læse fra inputfil et byte ad gangen for hvert byte skal vi kigge i Code tabellen og skrive de bytes i outputfil   
+        int tempByte;
         // Opens File to read from 
-        try ( FileInputStream input = new FileInputStream(inputFile)) {
+        try ( FileInputStream input = new FileInputStream(inputFile);  FileOutputStream output = new FileOutputStream(outputFile);  BitOutputStream bitput = new BitOutputStream(output);) {
             // "For at læse bytes fra en fil, skal man bruge read-metoden fra FileInputStream" 
             // This read method reads 1 byte instead of 4 bytes like the read method of the library class BitINputstream 
-            while ((temp = input.read()) != -1) {
+            while ((tempByte = input.read()) != 0) {
                 // int temp = (int) flin.read(); 
-                System.out.println(temp);
-                alphabet[temp]++;
+                System.out.println(tempByte);
+
+                String currentCode = codeLookupTable[tempByte];
+                for (int i = 0; i < currentCode.length() - 1; i++) {
+                    bitput.writeBit(currentCode.charAt(i));
+
+                }
+
             }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
 
+    }
+    
+    
+    
     //Task 2) Use the Huffman Algorithm with the Occurrence Table as input ( Use all 256 entries, also those, that do not occur  
-    public DictBinTree huffmanAlgorithm(int[] alphabet) {
+    public DictBinTree huffmanAlgorithm2(int[] alphabet) {
 
         int n = alphabet.length;
 
@@ -130,109 +252,5 @@ public class Encode {
         // Might need change it to Node
     }
     
-     public BinNode huffmanAlgorithm2(int[] alphabet) {
-
-        int n = alphabet.length;
-
-        //minHeap
-        PQ priorityQueue = new PQHeap();
-
-        // Q = C 
-        // Initialize the Priority Queue with the Characters 
-        for (int i = 0; i < n - 1; i++) {
-
-            BinNode node = new BinNode(i);
-
-            // Add element into tree? 
-            node.key = i ; // Not sure if correct          // Should this be changed ? 
-
-            // Add a tree
-            Element e = new Element(alphabet[i], node);
-
-            priorityQueue.insert(e);
-
-        }
-
-        // for i = 1 to n-1 
-        // Not entirely sure how it translates from pseudo to our yet. So I will just minus it as usual 
-        // I just guess we start @Index 0  and then move to second last index which is length -2 in java ArrayList
-        for (int i = 0; i < n - 2; i++) {
-
-            // Doesn't this provide us with a tree instead of a node?
-            // Should be tree or node? hmmm
-            // Extract the elements with the lowest key in the heap
-            // and get their sum
-            Element x = priorityQueue.extractMin();
-            Element y = priorityQueue.extractMin();
-            int sum = x.getKey() + y.getKey();
-            System.out.println(sum);
-            // Used later to get the nodes left 
-            BinNode xx = (BinNode) x.getData();
-            BinNode yy = (BinNode) y.getData();
-
-            // Creates new tree & adds the two previous as children
-            BinNode z = new BinNode(sum);
-            z.binNodeLeft =  (BinNode) x.getData();
-            z.binNodeRight =(BinNode) y.getData();
-
-            // Adds the new tree to the priority queue
-            priorityQueue.insert(new Element(sum, z));
-        }
-
-        return (BinNode) priorityQueue.extractMin().getData();
-    }
-
-    //Task 3 )  Convert the Huffman Tree to a Table(An Array with 256 entries)
-    // Over the codes for each of the possible bytes 
-    // Remember - The bytes are of the type int between 0 and 255 
-    // and can be used as indexes in the arrays
-    public String[] saveCode(DictBinTree tree) {
-        ArrayList<String> code = tree.in_order_walk_with_path();
-        String[] codeArray = new String[256];
-        // make toArray later
-
-        for (int i = 0; i < code.size() - 1; i++) {
-            codeArray[i] = code.get(i);  
-            System.out.println(codeArray[i]);
-        }
-        codelookup = codeArray;
-        return codeArray;
-    }
-
-    //task 4)  Write alphabeth into outputfile
-    public void writeIntsTOoOutput(int[] alphabeth, String outFile) throws IOException {
-        try ( FileOutputStream output = new FileOutputStream(outFile);  BitOutputStream out = new BitOutputStream(output);) {
-            for (int i : alphabeth) {
-                out.writeInt(i);
-            }
-        }
-
-    }
-
-    //task 5)   
-    public void WriteInput(String inputFile, String outputFile) throws IOException {
-
-        // Vi skal læse fra inputfil et byte ad gangen for hvert byte skal vi kigge i Code tabellen og skrive de bytes i outputfil   
-        int tempByte;
-        // Opens File to read from 
-        try ( FileInputStream input = new FileInputStream(inputFile);  FileOutputStream output = new FileOutputStream(outputFile);  BitOutputStream bitput = new BitOutputStream(output);) {
-            // "For at læse bytes fra en fil, skal man bruge read-metoden fra FileInputStream" 
-            // This read method reads 1 byte instead of 4 bytes like the read method of the library class BitINputstream 
-            while ((tempByte = input.read()) != 0) {
-                // int temp = (int) flin.read(); 
-                System.out.println(tempByte);
-
-                String currentCode = codelookup[tempByte];
-                for (int i = 0; i < currentCode.length() - 1; i++) {
-                    bitput.writeBit(currentCode.charAt(i));
-
-                }
-
-            }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
 
 }
