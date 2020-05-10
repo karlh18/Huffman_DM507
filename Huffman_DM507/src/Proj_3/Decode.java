49286fa99829e39c422fcdb5aadc39122ff90c0e
karlh18 <43171793+karlh18@users.noreply.github.com>
2020-05-10 16:12:34 +0100

    __ALmost Done

--- Huffman_DM507/src/Proj_3/Decode.java
+++ Huffman_DM507/src/Proj_3/Decode.java
@@ -15,38 +15,36 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
- * @author pradeepthayaparan
+ * @author Karl Amadeus Finsson Hansen - karlh18@student.sdu.dk
+ * @author Pradeep Thayaparan - prtha18@student.sdu.dk
+ * @author Lavan Sathiyaseelan - lasat17@student.sdu.dk
  */
 public class Decode {
     
-  int[] occurenceTable = new int[256];
-    public static void main(String[] args) throws IOException {
-        Decode decode = new Decode();
+  static int[] alphabet = new int[256];
+  static int byteAmount = 0;
+ 
+  
+  
+  
+  public static void main(String[] args) throws IOException {
+       
+        // Open input and output byte streams to/from files.
+        FileInputStream inFile = new FileInputStream("hej_zippy.txt");
+        FileOutputStream outFile = new FileOutputStream("hejsa.txt");
 
-        String inputFile = "hej_zippy.txt";
-        String outputFile = "hejsa.txt";
+        // Wrap the new bit streams around the input/output streams.
+        BitInputStream in = new BitInputStream(inFile);
+        
+        // Reads the occurencetable from the inputfile & stores them in an integer array
+        alphabet = readoccurenceTable(in);
+        
+        //Regenerates the Huffman-Tree from the occurencetable
+        BinNode huffmanNodes = huffmanAlgorithm(alphabet);
+        
+        // Writes 
         
         
-        FileInputStream finput = new FileInputStream(inputFile);  
-        FileOutputStream foutput = new FileOutputStream(outputFile);
-        BitInputStream binput = new BitInputStream(finput); 
-        BitOutputStream boutput = new BitOutputStream(foutput);
-
-        decode.occurenceTable = decode.readoccurenceTable(binput);
-
-        System.out.println("104 "+decode.occurenceTable[104]);
-        System.out.println("101 "+decode.occurenceTable[101]);
-        System.out.println("106 "+decode.occurenceTable[106]);
-
-        BinNode huffmantree = decode.huffmanAlgorithm(decode.occurenceTable); 
-        
-        DictBinTree dictBinTree = new DictBinTree();   
-         
-        dictBinTree.root = huffmantree;
-        
-         
-         decode.treewalk(huffmantree, binput, foutput);       
     }
     
 
@@ -54,180 +52,92 @@ public class Decode {
     
   
 
-    // task 1  read  the Occurence table from the inputfile for the 256 bytes. It works
-    public int[] readoccurenceTable(BitInputStream  bitInput) throws IOException {
+    // Task 1)  reads  the Occurence table(Hyppighedstabellen) from the inputfile for the 256 bytes. & Calculates the number of bytes read
+    public static int[] readoccurenceTable(BitInputStream bitInput) throws IOException {
         
-        int temp;
-        // Opens File to read from 
-        try  {
-            // "For at læse bytes fra en fil, skal man bruge read-metoden fra FileInputStream" 
-            // This read method reads 1 byte instead of 4 bytes like the read method of the library class BitINputstream 
-
-            for (int i = 0; i < occurenceTable.length ; i++) {
-
-                occurenceTable[i] = bitInput.readInt(); 
-          //       System.out.println(occurenceTable[i]);
-
+        int[] occurenceTable = new int[256];
+        
+        try {
+            for (int i = 0; i < occurenceTable.length; i++) {
+                occurenceTable[i] = bitInput.readInt(); // This read method reads 1 byte and stores it in the correct index in the occurrence table
+                byteAmount +=occurenceTable[i]; // Creates byteAmount 
             }
-            // int temp = (int) flin.read();   
-
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
         }
-
         return occurenceTable;
-    }  
+    }
     
-    // Task 2: regenerate a huffman tree.  
-  public BinNode huffmanAlgorithm(int[] alphabet) {
+    // Task 2) Regenerates the huffman Tree  
+    public static BinNode huffmanAlgorithm(int[] alphabet) {
 
-        int n = alphabet.length;
+        int n = alphabet.length; // length of the alphabet
 
         //minHeap
         PQ priorityQueue = new PQHeap();
 
-        // Q = C 
-        // Initialize the Priority Queue with 256 items aka the Characters to the queue 
-        for (int i = 0; i < n - 1; i++) {
+        // Q = C : Initialize the Priority Queue with 256 items aka the Characters to the queue 
+        for (int i = 0; i < n; i++) {
 
-            // Each node represents a character 
+            // Each node represents a character(byte)
             BinNode node = new BinNode(i);
 
             // Add an Element to the queue:  Frequency as Key in Element & the Tree/Node as data 
             Element e = new Element(alphabet[i], node);
             priorityQueue.insert(e);
-
         }
 
-        // ########CAN BE DELETE VERY SOOOON###############################
-//        int counter = 0; // used for test
-        // for i = 1 to n-1 
-        // Not entirely sure how it translates from pseudo to our yet. So I will just minus it as usual 
-        // I just guess we start @Index 0  and then move to second last index which is length -2 in java ArrayList
-        // ################################################################
         // For loop: Takes everything out of the Priority Queue except for the last one
-        for (int i = 0; i < n - 2; i++) {
+        for (int i = 0; i < n - 1; i++) {
 
             // Extract the 2 elements with the lowest frequency(key) in the heap
             Element x = priorityQueue.extractMin();
             Element y = priorityQueue.extractMin();
 
-            int sum = x.getKey() + y.getKey(); // sum used as key for a newly created Node, which will be set as parent of the two extracted nodes. 
+            int sum = x.getKey() + y.getKey(); // sum used as key for a newly created Node, which will have the two extracted nodes as children  
 
-//             System.out.println("counter: " + counter++ + " sum : " + sum); // Used for test ##############Can be deleted soon
-            // Creates new node/tree & adds the two extracted nodes
-            BinNode z = new BinNode(sum);   // Not sure if this should have anything  or if it should have 0 ???  Or does it not matter?
+            // Creates new node & adds the two extracted nodes as children to it
+            BinNode z = new BinNode(sum); // The sum of this one is irrelevant. 
             z.binNodeLeft = (BinNode) x.getData();
             z.binNodeRight = (BinNode) y.getData();
 
-            // Adds the new node/tree to the priority queue
+            // Adds the new node to the priority queue
             priorityQueue.insert(new Element(sum, z));
         }
-
-        return (BinNode) priorityQueue.extractMin().getData();
+        return (BinNode) priorityQueue.extractMin().getData(); // Returns the root of the tree
     }
 
-    public int calcBytes(int[] bytes) {
+    
+    
+    // Task 3) We use the Regenerated huffman tree to decode the bits in the inputfile by traversing down the root until we meet a leaf
+        // When a leaf has been found we write its key to the outputFile
+    public static void treewalk(BinNode rootNode, BitInputStream inputStream, FileOutputStream fileoutput) throws IOException {
 
-        int sum = 0;
-
-        for (int i = 0; i < bytes.length; i++) {
-
-            sum += bytes[i];
-        }
-
-        return sum;
-    }
-
-    public int[] decodingBits(int[] freqTable, String inputFile, String outputFile ) {
-
-         int bitsofar =0;
-         int totalbits = calcBytes(freqTable);   
-         List<Integer> list = new ArrayList<>();
-              
-         String s =""; 
-        try(FileInputStream input = new FileInputStream(inputFile); 
-            BitInputStream bitInput = new BitInputStream(input);   
-            FileOutputStream output = new FileOutputStream(outputFile);
-            BitOutputStream bitput = new BitOutputStream(output);) {
-            
-            
-            while(bitsofar < totalbits){
-            
-             s += bitInput.readBit();    
-                
-          
-             for(int i = 0; i< freqTable.length-1; i++){
-             
-                if(s.equals(freqTable[i])){
-                   list.add(i);
-               //    bitput.writeBit(i); 
-                   s = "";
-                }
-             }
-             
-              
-            
-            }
-//          for( int i = 0; i < bytes.length-1 ; i++){
-//          bytes[i] = bitInput.readBit();  
-//          }  
-//         
-            
-            
-        } catch (Exception e) {
-        }
-        
-
-           return freqTable;  
-    }
-
-     public void treewalk(BinNode rootNode, BitInputStream inputStream, FileOutputStream fileoutput) throws IOException {
-
-        int counter = 0;
-        int numBytes =calcBytes(occurenceTable);
-         System.out.println("occurrenceTable: " + occurenceTable[106]);
-        BinNode resetBinNode = rootNode;  
+        int counter = 0; // Keeps track of how many bytes we have written so far so we know when to stop. 
+        BinNode resetBinNode = rootNode; // 
         String bitprefix = "";
         int readBit;
-        
-        
-        
-//        for(int i = 0; i<32; i++){
-//            readBit = inputStream.readBit();
-//        }
-       
-        while ((counter < numBytes) && (readBit = inputStream.readBit()) != -1) {
+
+        while ((counter < byteAmount) && (readBit = inputStream.readBit()) != -1) {
             if (rootNode.binNodeLeft == null && rootNode.binNodeRight == null) {
                 System.out.println("BitPrefix: " + bitprefix);
-                fileoutput.write(rootNode.key); 
+                fileoutput.write(rootNode.key);
                 System.out.println("rootNode.key: " + rootNode.key);
-                counter ++;
+                counter++;
                 // reset
-                bitprefix ="";
+                bitprefix = "";
                 rootNode = resetBinNode;
-                
+
             }
-            
-
-//            readBit = inputStream.readBit();
-//            if (readBit == -1){
-//                break;
-//            }
-            System.out.println("readbit: " + readBit);
+            // If currentbit = 0 then write 0 to outputfile
             if (readBit == 0) {
-
                 rootNode = rootNode.binNodeLeft;
                 bitprefix += readBit;
+            // if readBit = 1 then write 1 to outputfile
             } else if (readBit == 1) {
                 rootNode = rootNode.binNodeRight;
                 bitprefix += readBit;
-
-            }  
-            
-            
-
+            }
         }
-     }
+    }
 }
- 
