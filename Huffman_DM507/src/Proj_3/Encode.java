7c61f4d01b40d2570a2d55f6e7aaf6af8b6852a9
karlh18 <43171793+karlh18@users.noreply.github.com>
2020-05-10 14:24:10 +0100

    _update

--- Huffman_DM507/src/Proj_3/Encode.java
+++ Huffman_DM507/src/Proj_3/Encode.java
@@ -36,41 +36,16 @@ public class Encode {
         countFrequencyOfBytes(inFile); //Reads a file anc checks how frequent a given byte occurs
         
         //Generates a Huffman-Tree
-        BinNode huffmanTree = huffmanAlgorithm(alphabet);
+        BinNode huffmanNodes = huffmanAlgorithm(alphabet);
+        DictBinTree huffmanTree = new DictBinTree();
+        huffmanTree.root = huffmanNodes;
+        
+        // Generates a CodeLookupTable from the huffmanTree
+        codeLookupTable = createCodeLookupTable(huffmanTree);
         
         
         
         
-        
-
-//        // #Test2 - huffmanAlgorithm()
-//        BinNode binNode = encode.huffmanAlgorithm(encode.alphabet);  // works 
-//        // Uses the created graph  & adds it to the tree
-//        encode.huffManTree = new DictBinTree();
-//        encode.huffManTree.root = binNode;
-//
-//        //Test 3a:
-////        String[] dog = encode.huffManTree.in_order_walk_with_path();
-//
-//
-//
-//
-//        // String[255] is empty !!! Missing one assignment 
-//        String[] lookupTable = encode.createCodeLookupTable(encode.huffManTree);
-//
-//
-//
-//
-//
-////        System.out.println("104: " + dog[104]); 
-////        System.out.println("101: " + dog[101]); 
-////        System.out.println("106: " + dog[106]); 
-////        System.out.println("13: " + dog[13]); 
-////        System.out.println("10: " + dog[10]); 
-////        System.out.println("200: " + dog[200]); 
-//
-//        // Test 3b:
-////        encode.createCodeLookupTable(encode.huffManTree);
 //        // Test 4:
 //        // Tests that need to be conducted:
 //        // Check to see how many bytes are written 
@@ -91,7 +66,6 @@ public class Encode {
         int temp;  // Used to temporary store the current read() int
 
         while ((temp = input.read()) != -1) {// Uses the read method from FileInputStream  to read a byte (8bits) from a file at a time
-            System.out.println(temp); // used to test if it reads correct
             alphabet[temp]++;
         }
     }
@@ -138,54 +112,29 @@ public class Encode {
 
     
     
-    //Task 3 )  Convert the Huffman Tree to a Table(An Array with 256 entries)
-    // Over the codes for each of the possible bytes 
-    // Remember - The bytes are of the type int between 0 and 255 
-    // and can be used as indexes in the arrays
-    public void createCodeLookupTable(DictBinTree tree) {
+    //Task 3 )  Converts the Huffman Tree to a Code Lookup Table (An Array with 256 entries)
+        // Each byte will thus have a String similar to : "101"    
+    public static String[] createCodeLookupTable(DictBinTree tree) {
+        // Uses the code from the DictBinTree that traverses through the tree
         String[] code = tree.in_order_walk_with_path();
-        codeLookupTable = code;
+        return code;
   }
 
     
 
-    
-    
-    
-    
-    
-    // Create Test:  Check if there has been written 256 bytes * 4 
-    // 256*4 =  1024 bytes      // FileInputStream to check each byte  --- since the BitInputStream takes in 32 bytes
-    // 4 bits 
-    //task 4)  Write alphabet into outputfile
-    public void writeIntsToOutput(int[] alphabeth, String outFile) throws IOException {
-        try ( FileOutputStream output = new FileOutputStream(outFile);  BitOutputStream out = new BitOutputStream(output);  FileInputStream in = new FileInputStream(outFile)) {
-            int sumofbytes = 0;
-//            int counter = 0;
-            for (int i : alphabeth) {
-
-                out.writeInt(i);     //  
-                in.read(); 
-               
-                //    sumofbytes +=i;   
-                //     sumofbytes+= (int) out.writeInt(i);
-//                System.out.println("Counter: " + counter++);
-            }
-           
-            
-            System.out.println("Sum of bytes " + sumofbytes);
-            System.out.println("Alphabeth " + alphabet.length);
-
-        }
-
-    }
    
     // new method --> scan input     
-    public void writeToOutPut(int[] alphabeth, String infile, String outFile) throws FileNotFoundException, IOException {
-        FileOutputStream output = new FileOutputStream(outFile);
-        BitOutputStream out = new BitOutputStream(output);
-        FileInputStream in = new FileInputStream(infile);  
-        
+    
+    
+     // Task 4 (Write alphabet into outputfile) & 5
+    
+// Figure out how to write correct bits each time
+    // Figure out if should use Stream as parameter so it doesn't overrride the other method
+    // Test file if it works as intended 
+    //task 5)   Read the inputfile again 
+    // For each byte look at that int write those bits into the output file 
+    // "101"  -> writeBit(1), writeBit(0), write(1) 
+    public static  void writeToOutPut(int[] alphabeth,FileInputStream in, FileOutputStream output, BitOutputStream out) throws FileNotFoundException, IOException {
         // takes the 256 integers representing occurrences and writes dem down.
         // then the input file is scanned where each char is read to find its codeword in order, which then will be printed to the output file.  
         try {
@@ -193,86 +142,33 @@ public class Encode {
                 out.writeInt(i);
 
             }
-        } catch (Exception e) { 
-           e.printStackTrace();
-        
-        }  
-          System.out.println("104 "+ codeLookupTable[104]);
-          System.out.println("101 "+ codeLookupTable[101]);
-          
-          System.out.println("106 "+ codeLookupTable[106]);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        System.out.println("104 " + codeLookupTable[104]);
+        System.out.println("101 " + codeLookupTable[101]);
+
+        System.out.println("106 " + codeLookupTable[106]);
         while (in.available() != 0) {
 
             int i = in.read();
-          
-            System.out.println("LOOKUP "+codeLookupTable[i]);  
-            
-            
+
+            System.out.println("LOOKUP " + codeLookupTable[i]);
+
             char[] temp = codeLookupTable[i].toCharArray();
 
-           
             for (char c : temp) {
-                
-//               if(temp[i]=='1'){
-//               
-//                   System.out.println("TJEK");
-//               
-//               } else if (temp[i]=='0')
-//                    System.out.println("NO TJEK");
-                
-                out.writeBit(Character.getNumericValue(c));    
-                
-               
-                System.out.println("char : "+c+" Value = "+c + " Value of the char "+Character.getNumericValue(c));
-                //
+
+                out.writeBit(Character.getNumericValue(c));
+
+                System.out.println("char : " + c + " Value = " + c + " Value of the char " + Character.getNumericValue(c));
             }
-     
-       
-        } 
+
+        }
         
         in.close();
         out.close();
 
     }
-
-    // Figure out how to write correct bits each time
-    // Figure out if should use Stream as parameter so it doesn't overrride the other method
-    // Test file if it works as intended 
-    //task 5)   Read the inputfile again 
-    // For each byte look at that int write those bits into the output file 
-    // "101"  -> writeBit(1), writeBit(0), write(1) 
-    public void writeCodeWordsToOutput(String inputFile, String outputFile) throws IOException {
-        int tempByte;
-
-        // Opens File to read from 
-        try ( FileInputStream input = new FileInputStream(inputFile); 
-                FileOutputStream output = new FileOutputStream(outputFile);  
-                BitOutputStream bitput = new BitOutputStream(output);) {
-            // This read method reads 1 byte instead of 4 bytes like the read method of the library class BitINputstream 
-            while ((tempByte = input.read()) != -1) {
-                // int temp = (int) flin.read(); 
-                System.out.println(tempByte);
-                String currentCode = codeLookupTable[tempByte]; // "101" 
-                System.out.println(currentCode);
-                int j;
-                for (int i = 0; i < currentCode.length() - 1; i++) {
-                    if (currentCode.charAt(i) == '1') {
-                        System.out.println(currentCode.charAt(i));
-                        bitput.writeBit(1);
-                    } else {
-                        System.out.println(currentCode.charAt(i));
-                        bitput.writeBit(0);
-                    }
-//                    j = currentCode.charAt(i);
-//                    System.out.println(j);
-//                    bitput.writeBit(1);
-                }
-
-            }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Encode.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }
-
 }
